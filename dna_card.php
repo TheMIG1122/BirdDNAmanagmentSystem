4e1ea5cb37f2fe20fc3f959b215408ae039b2563
TheMIG1122 <themig1122@gmail.com>
2021-09-27 21:36:24 +0500

    Final Phase

--- dna_card.php
+++ dna_card.php
@@ -15,42 +15,42 @@
 				<!-- <div class="pos" id="_0:0" style="top:0">
 					<img name="_1100:850" src="assets/images/card_page.jpg" height="1100" width="850" border="0" usemap="#Map">
 				</div> -->
-				<div class="pos" id="_682:76" style="top:76;left:682">
+				<div class="pos" id="_682:76" style="top: 25px;left: 615;">
 					<span id="_15.0" style=" font-family:Arial; font-size:15.0px; color:#000000">
 					    <?php echo $dna_id?>
                     </span>
 				</div>
-				<div class="pos" id="_170:148" style="top:148;left:170">
+				<div class="pos" id="_170:148" style="top: 101px;left: 130px;">
 					<span id="_15.0" style=" font-family:Arial; font-size:15.0px; color:#000000">
                     <?php echo $bird_id?>
                     </span>
 				</div>
-				<div class="pos" id="_170:174" style="top:174;left:170">
+				<div class="pos" id="_170:174" style="top: 127px;left: 130;">
 					<span id="_13.5" style=" font-family:Arial; font-size:13.5px; color:#000000">
                     <?php echo $owner_name?>
                     </span>
 				</div>
-				<div class="pos" id="_170:200" style="top:200;left:170">
+				<div class="pos" id="_170:200" style="top: 151px;left: 130;">
 					<span id="_13.1" style=" font-family:Arial; font-size:13.1px; color:#000000">
                     <?php echo $specie?>
                     </span>
 				</div>
-				<div class="pos" id="_170:227" style="top:227;left:170">
+				<div class="pos" id="_170:227" style="top: 177px;left: 130;">
 					<span id="_13.5" style=" font-family:Arial; font-size:13.5px; color:#000000">
                     <?php echo convert_date($created_at)?>
                     </span>
 				</div>
-				<div class="pos" id="_447:227" style="top:227;left:447">
+				<div class="pos" id="_447:227" style="top: 172px;left: 385px;">
 					<span id="_15.0" style=" font-family:Arial; font-size:15.0px; color:#000000">
                     <?php echo $type?>
                     </span>
 				</div>
-				<div class="pos" id="_170:253" style="top:253;left:170">
+				<div class="pos" id="_170:253" style="top: 200px;left: 130;">
 					<span id="_13.5" style=" font-family:Arial; font-size:13.5px; color:#000000">
                     <?php echo convert_date($updated_at)?>
                     </span>
 				</div>
-				<div class="pos" id="_447:253" style="top:253;left:447">
+				<div class="pos" id="_447:253" style="top: 197px;left: 386px;">
 					<span id="_15.0" style=" font-family:Arial; font-size:15.0px; color:#000000">
                     <?php echo $result?>
                     </span>
