b0bc86c728d24fa1a00199d3a57e6785298b28e7
TheMIG1122 <themig1122@gmail.com>
2021-10-02 00:54:14 +0500

    SQL Updated dna_samples.dna_id to dna_sample.owner_id

--- SQL/tech_roar_bird_dna_managementv2.sql
+++ SQL/tech_roar_bird_dna_managementv2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 01, 2021 at 09:48 PM
+-- Generation Time: Oct 01, 2021 at 09:53 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 7.4.14
 
