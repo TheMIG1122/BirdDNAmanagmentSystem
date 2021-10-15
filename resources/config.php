74a4db7d64af0ebd1761e6e60aae10775de5aae7
TheMIG1122 <themig1122@gmail.com>
2021-10-15 23:34:41 +0500

    DB name Change

--- resources/config.php
+++ resources/config.php
@@ -12,7 +12,8 @@ defined('base_url') ? null : define('base_url', 'http://localhost/10.100.1/Septe
 // Local
 defined('DB_USER') ? null : define('DB_USER','root' );
 defined('DB_PASS') ? null : define('DB_PASS', '' );
-defined('DB_NAME') ? null : define('DB_NAME', 'qualitydnalabcom_qualitydnalab' );
+// defined('DB_NAME') ? null : define('DB_NAME', 'qualitydnalabcom_qualitydnalab' );
+defined('DB_NAME') ? null : define('DB_NAME', 'techroar_birddna' );
 
 
 $connection = mysqli_connect(DB_HOST , DB_USER , DB_PASS , DB_NAME);
